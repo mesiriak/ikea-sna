09254c9cf5ec87a1c3e243f15a84f23d239a16fa
Max <80003087+mesiriak@users.noreply.github.com>
2023-02-18 19:25:32 +0200

    hf: json convertor load

--- convertors/json_conv.py
+++ convertors/json_conv.py
@@ -15,10 +15,15 @@ def save(data: list[dict[str, Any]], path: str, name: str = None) -> None:
     preload = []
 
     if os.path.exists(path):
-        preload = json.load(open("products.json", "r", encoding="utf-8"))
+        preload = json.load(open(path, "r", encoding="utf-8"))
 
-    with open("products.json", "w", encoding="utf-8") as file:
+    with open(path, "w", encoding="utf-8") as file:
         preload.extend(data)
         json.dump(preload, file, indent="\t", ensure_ascii=False)
 
     return None
+
+
+def load(path: str) -> list[dict[str, Any]]:
+    with open(path) as file:
+        return json.load(file)
